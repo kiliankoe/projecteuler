1f9f9d80c8e68499b1b6605f98f18390cb795862
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-08-24 23:54:48 +0200

    Swift: 003
    Runs in less than a second compiled, takes quite a while as a playground

---
+++ 003/003.playground/section-1.swift
@@ -0,0 +1,31 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143?
+
+extension Int {
+    func isPrime() -> Bool {
+        for var i = 2; i < self; i++ {
+            if self % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+var num = 600851475143
+var factors = [Int]()
+
+while num >= 2 {
+    for i in 2...num {
+        if i.isPrime() && num % i == 0 {
+            println("\(num) / \(i)")
+            num /= i
+            println(num)
+            factors.append(i)
+            break
+        }
+    }
+}
+
+println(factors)

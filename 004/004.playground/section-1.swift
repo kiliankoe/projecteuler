a2507fa6d52e6950df21f2c386aa899613a3848e
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-08-25 19:54:04 +0200

    Swift: 004

---
+++ 004/004.playground/section-1.swift
@@ -0,0 +1,39 @@
+//A palindromic number reads the same both ways. The largest palindrome made
+//from the product of two 2-digit numbers is 9009 = 91 × 99.
+//
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+func isPalindrome(number: Int) -> Bool {
+    var rev = 0
+    var num = number
+
+    while num != 0 {
+        rev = rev * 10 + num % 10
+        num = num / 10
+    }
+
+    if rev == number {
+        return true
+    }
+    return false
+}
+
+let range = 999
+var palindromes = [Int]()
+
+for var a = range; a > 0; a-- {
+    for var b = range; b > 0; b-- {
+        if isPalindrome(a * b) {
+            palindromes.append(a * b)
+        }
+    }
+}
+
+var biggest = 0
+for i in palindromes {
+    if i > biggest {
+        biggest = i
+    }
+}
+
+println(biggest)
